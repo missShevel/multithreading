d5c4c80beee573173f09264f81a4d079e691ef39
Olha <o.v.shevel@gmail.com>
2023-03-16 00:05:03 +0200

    blocking

--- src/main/java/lab1/Counter.java
+++ src/main/java/lab1/Counter.java
@@ -3,11 +3,17 @@ package lab1;
 public class Counter {
     private int value;
 
-    public synchronized void  increment() {
+    public synchronized void increment() throws InterruptedException {
+        while (value > 100000) wait();
         value += 1;
+        notifyAll();
+
     }
-    public synchronized void decrement() {
+    public synchronized void decrement() throws InterruptedException {
+        while (value < -100000) wait();
         value -= 1;
+        notifyAll();
+
     }
     int getValue() {
         return this.value;
--- src/main/java/lab1/CountingThread.java
+++ src/main/java/lab1/CountingThread.java
@@ -11,15 +11,53 @@ public class CountingThread extends Thread {
     }
 
     @Override
+//    public synchronized void run() {
+//        int i = 0;
+//        while (i < repeats) {
+//            while (counter.getValue() != 0) {
+//                try {
+//                    wait();
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//            if (action == '+') {
+//                counter.increment();
+//                i++;
+//                while (counter.getValue() != 0) {
+//                    try {
+//                        wait();
+//                    } catch (InterruptedException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//                if (action == '-') {
+//                    counter.decrement();
+//                    i++;
+//                }
+//                notifyAll();
+//
+//                System.out.println("Value: " + counter.getValue());
+//            }
+//        }
+//    }
     public void run() {
-        for (int i = 0; i < repeats; i++){
+        for (int i = 0; i < repeats; i++) {
             if (action == '+') {
+                try {
                     counter.increment();
-
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             } else {
+                try {
                     counter.decrement();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
             System.out.println("Value: " + counter.getValue());
         }
+
     }
 }
